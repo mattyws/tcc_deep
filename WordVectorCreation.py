02a62436bfc0fc942128c27c52929995982443f2
mattyws <mattyws@gmail.com>
2017-11-17 12:35:35 -0300

    Changing scripts

--- WordVectorCreation.py
+++ WordVectorCreation.py
@@ -16,10 +16,10 @@ tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 stop_set = nltk.corpus.stopwords.words(language)
 stemmer = gensim.parsing.PorterStemmer()
 mongodb = MongoLoadDocumentMeta('patents')
-documents = mongodb.get_all_meta('testing_docs100')
+documents = mongodb.get_all_meta('training_docs100')
 corpus = MongoLoadDocumentData('patents', documents, clean_text=True, tokenizer=tokenizer, stop_set=stop_set,description=True)
 
-word2vec_model = dl.learn.Word2VecTrainer().load_model('../word2vec_mongo.model')
+word2vec_model = dl.learn.Word2VecTrainer().load_model('word2vec_mongo.model')
 word_vector_generator = dl.data_representation.Word2VecEmbeddingCreator(word2vec_model, maxWords=200, embeddingSize=200)
 
 for document in documents:
@@ -29,7 +29,7 @@ for document in documents:
     word_embedding_matrix = word_vector_generator.create_x_text(content)
     client = pymongo.MongoClient()
     patents_database = client.patents
-    word_embedding_collection = patents_database.documents_embedding_docs100
+    word_embedding_collection = patents_database.training_embedding_docs100
     document['embedding'] = bson.binary.Binary(pickle.dumps(word_embedding_matrix, protocol=2))
     word_embedding_collection.insert_one(document)
     # fs = gridfs.GridFS(patents_database, collection="documents_embedding_docs100")
