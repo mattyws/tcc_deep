ad4728346a3f718a21ce509e9fd40085ffa8266f
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-15 20:53:46 -0300

    Changing doc2vec scripts

--- mongodb_scripts/shuffle_collection.py
+++ mongodb_scripts/shuffle_collection.py
@@ -7,14 +7,18 @@ path = 'base100'
 
 client = pymongo.MongoClient()
 patents_database = client.patents
-collection = patents_database.training_docs100
-shuffled_collection = patents_database.shuffled_training_docs100
+collection = patents_database.testing_embedding_old
+shuffled_collection = patents_database.shuffled_testing_embedding_old
 
 all_documents = collection.find({}, {"filename":1})
 shuffled = []
 for doc in all_documents:
     shuffled.append(doc["filename"])
 shuffle(shuffled)
+i=0
 for doc in shuffled:
+    if i%1000 == 0:
+        print(str(i) + ' ' + doc)
     patent = collection.find_one({"filename":doc})
     shuffled_collection.insert_one(patent)
+    i+=1
