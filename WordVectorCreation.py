6a4872a812d318e33c95a5284bf90901d9876962
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-16 00:04:31 -0300

    Changing doc2vec scripts

--- WordVectorCreation.py
+++ WordVectorCreation.py
@@ -19,7 +19,7 @@ mongodb = MongoLoadDocumentMeta('patents')
 documents = mongodb.get_all_meta('testing_docs100')
 corpus = MongoLoadDocumentData('patents', documents, clean_text=True, tokenizer=tokenizer, stop_set=stop_set,description=True)
 
-word2vec_model = dl.learn.Word2VecTrainer().load_model('../word2vec_models/word2vec_mongo.model')
+word2vec_model = dl.learn.Word2VecTrainer().load_model('word2vec_mongo.model')
 word_vector_generator = dl.data_representation.Word2VecEmbeddingCreator(word2vec_model, maxWords=150, embeddingSize=200)
 
 i=0
