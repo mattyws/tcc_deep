442999a7894b796703a2c19c7dd6c84bb28dd705
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-26 14:17:34 -0300

    Change scripts for testing

--- TrainLSTMMongoDBSection.py
+++ TrainLSTMMongoDBSection.py
@@ -21,10 +21,10 @@ Configurations
 maxWords = 150
 embeddingSize = 400
 timer = TimerCounter() # Timer to count how long it takes to perform each process
-training_documents_collection = 'training_embedding_mongo_400'
-testing_documents_collection = 'testing_embedding_mongo_400'
-model_saved_name = "../TrainedLSTM/keras_rnn_mongo_400.model"
-result_file_name = "../TrainedLSTM/results/result_rnn_mongo_400"
+training_documents_collection = 'training_embedding_old_400'
+testing_documents_collection = 'testing_embedding_old_400'
+model_saved_name = "../TrainedLSTM/keras_rnn_old_400.model"
+result_file_name = "../TrainedLSTM/results/result_rnn_old_400"
 epochs = 20
 layers = 2
 
