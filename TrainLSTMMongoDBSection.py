1c1b7df68a0a1b2546d663de6cb46ec5a4aa0328
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-29 20:12:25 -0300

    Change scripts for testing

--- TrainLSTMMongoDBSection.py
+++ TrainLSTMMongoDBSection.py
@@ -19,7 +19,7 @@ from DeepLearning.helper import TimerCounter, classMap
 Configurations
 '''
 maxWords = 150
-embeddingSize = 50
+embeddingSize = 400
 timer = TimerCounter() # Timer to count how long it takes to perform each process
 training_documents_collection = 'training_document_embedding_mongo_400'
 testing_documents_collection = 'testing_document_embedding_mongo_400'
