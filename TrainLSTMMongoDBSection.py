845701606e3a713cd270ee3c21dc8a2a3e76f0e2
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-30 13:19:43 -0300

    Change scripts for testing

--- TrainLSTMMongoDBSection.py
+++ TrainLSTMMongoDBSection.py
@@ -21,8 +21,8 @@ Configurations
 maxWords = 150
 embeddingSize = 50
 timer = TimerCounter() # Timer to count how long it takes to perform each process
-training_documents_collection = 'training_document_embedding_mongo_50_dif'
-testing_documents_collection = 'testing_document_embedding_mongo_50_dif'
+training_documents_collection = 'training_document_embedding_old_50_dif'
+testing_documents_collection = 'testing_document_embedding_old_50_dif'
 model_saved_name = "../TrainedNN/keras_nn_old_50_dif.model"
 result_file_name = "../TrainedNN/results/result_nn_old_50_dif"
 epochs = 12
