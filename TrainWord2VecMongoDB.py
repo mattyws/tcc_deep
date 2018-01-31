efa853d85c7325ffaf227c557a23f05057706a1f
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-01-31 10:15:51 -0300

    Change scripts for testing

--- TrainWord2VecMongoDB.py
+++ TrainWord2VecMongoDB.py
@@ -25,7 +25,7 @@ except getopt.GetoptError:
     print('test.py -m <model_file> -t')
     sys.exit(2)
 
-output_model_file = '../word2vec_models/word2vec_noDigits.model'
+output_model_file = '../word2vec_models/word2vec_old_300.model'
 new_model = True
 retrain = False
 input_model_file = ''
@@ -48,9 +48,9 @@ stop_set = nltk.corpus.stopwords.words(language)
 stemmer = gensim.parsing.PorterStemmer()
 mongodb = MongoLoadDocumentMeta('patents')
 documents = mongodb.get_all_meta('word2vec_old')
-all_corpus = MongoLoadDocumentData('patents', documents, clean_text=True, tokenizer=tokenizer, stop_set=stop_set, abstract=True, description=True, remove_digits=True)
-size = 200
-iteration = 20
+all_corpus = MongoLoadDocumentData('patents', documents, clean_text=True, tokenizer=tokenizer, stop_set=stop_set, abstract=True, description=True)
+size = 300
+iteration = 25
 
 # print("============================= Loading data =============================")
 # data_dict = database.FlatStructureDatabase('../../database/descriptions/base').subclasses()
