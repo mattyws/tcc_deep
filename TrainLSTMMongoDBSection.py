a6d85d11491c9c1e518ab80faae7c639314a460f
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-02-02 19:54:49 -0300

    Change scripts for testing

--- TrainLSTMMongoDBSection.py
+++ TrainLSTMMongoDBSection.py
@@ -19,13 +19,13 @@ from DeepLearning.helper import TimerCounter, classMap
 Configurations
 '''
 maxWords = 150
-embeddingSize = 50
+embeddingSize = 300
 timer = TimerCounter() # Timer to count how long it takes to perform each process
-training_documents_collection = 'training_embedding_old_50'
-testing_documents_collection = 'testing_embedding_old_50'
-model_saved_name = "../TrainedConv/keras_cnn_old_50.model"
-result_file_name = "../TrainedConv/results/result_cnn_old_50"
-epochs = 2
+training_documents_collection = 'training_document_embedding_mongo_300'
+testing_documents_collection = 'testing_document_embedding_mongo_300'
+model_saved_name = "../TrainedNN/keras_nn_mongo_300.model"
+result_file_name = "../TrainedNN/results/result_nn_mongo_300"
+epochs = 12
 layers = 1
 
 
@@ -57,11 +57,11 @@ embedding_generator = MongoDBMetaEmbeddingGenerator(documents, "section", class_
                                                     serve_forever=True)
 print("=============================== Create training classes ===============================")
 #Build a factory for a model adapter
-model_factory = dl.factory.factory.create('KerasCovolutionalNetwork', input_shape=(maxWords, embeddingSize))
+# model_factory = dl.factory.factory.create('KerasCovolutionalNetwork', input_shape=(maxWords, embeddingSize))
 # model_factory = dl.factory.factory.create('MultilayerKerasRecurrentNN', input_shape=(maxWords, embeddingSize),
 #                                                   numNeurouns=len(ipc_sections), numOutputNeurons=len(ipc_sections), layers=layers, use_dropout=True, dropout=0.5)
-# model_factory = dl.factory.factory.create('KerasMultilayerPerceptron', num_class=len(ipc_sections), input_dim=embeddingSize, layers=layers,
-#                                           hidden_units=[25])
+model_factory = dl.factory.factory.create('KerasMultilayerPerceptron', num_class=len(ipc_sections), input_dim=embeddingSize, layers=layers,
+                                          hidden_units=[25])
 model = model_factory.create()
 
 timer.start() #start a timer for training
