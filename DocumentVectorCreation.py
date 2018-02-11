03998273b07e1e7765d7a271b0ab03877af0c3e4
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-02-11 00:32:29 -0300

    Change scripts for testing

--- DocumentVectorCreation.py
+++ DocumentVectorCreation.py
@@ -31,7 +31,7 @@ shuffle(shuffled)
 
 i = 0
 for doc in shuffled:
-    document = mongodb.get_document_by(collection, 'filename', doc['filename'])
+    document = mongodb.get_document_by(collection, 'filename', doc)
     if i%1000 == 0:
         print(str(i) + ' ' + document['filename'])
     content = corpus.get_file_content(document['filename'])
