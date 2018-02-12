928f76912e8d8fdc647210843ca006bc368cd805
Mattyws Ferreira Grawe <mattyws@gmail.com>
2018-02-12 03:11:50 -0300

    Change scripts for testing

--- TrainLSTMMongoDBSection.py
+++ TrainLSTMMongoDBSection.py
@@ -19,12 +19,12 @@ from DeepLearning.helper import TimerCounter, classMap
 Configurations
 '''
 maxWords = 150
-embeddingSize = 200
+embeddingSize = 300
 timer = TimerCounter() # Timer to count how long it takes to perform each process
-training_documents_collection = 'training_document_embedding_mongo_200_2'
-testing_documents_collection = 'testing_document_embedding_mongo_200_2'
-model_saved_name = "../TrainedNN/keras_nn_mongo_200.model"
-result_file_name = "../TrainedNN/results/result_nn_mongo_200"
+training_documents_collection = 'training_document_embedding_mongo_300_2'
+testing_documents_collection = 'testing_document_embedding_mongo_300_2'
+model_saved_name = "../TrainedNN/keras_nn_mongo_300.model"
+result_file_name = "../TrainedNN/results/result_nn_mongo_300"
 epochs = 12
 layers = 1
 
